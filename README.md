bfb1ce14c80137ebc67877592bafac49159a0fb9
Emma <48389529+emjogale@users.noreply.github.com>
2022-01-23 12:04:55 +0000

    remove error messages if a valid email is entered

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Frontend Mentor - Maker pre-launch landing page solution
 
-This is a solution to the [Maker pre-launch landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/maker-prelaunch-landing-page-WVZIJtKLd). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+This is a solution to the [Maker pre-launch landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/maker-prelaunch-landing-page-WVZIJtKLd).
 
 ## Table of contents
 
@@ -13,10 +13,7 @@ This is a solution to the [Maker pre-launch landing page challenge on Frontend M
   - [What I learned](#what-i-learned)
   - [Continued development](#continued-development)
   - [Useful resources](#useful-resources)
-- [Author](#author)
-- [Acknowledgments](#acknowledgments)
-
-**Note: Delete this note and update the table of contents based on what sections you keep.**
+-
 
 ## Overview
 
--- starter-code/main.js
+++ starter-code/main.js
@@ -1,23 +1,28 @@
 // select the button and email elements
 const buttonEl = document.querySelector("#button");
 const emailEl = document.querySelector("#email");
-const errorMessageEl = document.querySelector(".message");
-console.log(errorMessageEl);
+const errorMessageEl = document.querySelectorAll(".message");
 // email checker helper function
 // check if an email is valid
 const isEmailValid = (email) => {
-	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	const re =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(email);
 };
 // email validator helper function
 const checkEmail = () => {
 	const email = emailEl.value.trim();
-	if (!isEmailValid(email)) {
-		console.log("nooooo not an email!!!");
-		errorMessageEl.classList.add("error-message");
+	if (!email) {
+		errorMessageEl[0].classList.remove("error-message");
+		errorMessageEl[1].classList.add("error-message");
+	} else if (!isEmailValid(email)) {
+		errorMessageEl[1].classList.remove("error-message");
+		errorMessageEl[0].classList.add("error-message");
 		emailEl.classList.add("error-message");
 	} else {
 		// success(emailEl);
+		errorMessageEl[0].classList.remove("error-message");
+		errorMessageEl[1].classList.remove("error-message");
 		console.log("form validated!!!");
 	}
 };
